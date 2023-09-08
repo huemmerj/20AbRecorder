3d04caa261badeb4c204f3602d89c64936585b3a
Jens Hümmer <huemmer.jens@gmail.com>
2023-09-08 18:23:38 +0200

    added table

---
+++ my-app/src/routes/game/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Header from './Header.svelte';
+	import Body from './Body.svelte';
+	/**
+	 * @type {string[]}
+	 */
+	let player = ['Jens'];
+	let started = false;
+</script>
+
+<table>
+	<thead>
+		{#each player as p}
+			<th>{p}</th>
+		{/each}
+		<th />
+		<Header
+			on:addPlayer={(name) => {
+				console.log(player);
+				player = [...player, name.detail];
+			}}
+			{started}
+		/>
+	</thead>
+	<Body {player} {started} />
+</table>
+
+{#if started}
+	<p>Game started</p>
+{:else}
+	<button
+		on:click={() => {
+			started = true;
+		}}>Start Game</button
+	>
+{/if}
---
+++ my-app/src/routes/game/Body.svelte
@@ -0,0 +1,21 @@
+<script>
+	/**
+	 * @type {string[]}
+	 */
+	export let player;
+
+	/**
+	 * @type {boolean}
+	 */
+	export let started = false;
+</script>
+
+<tbody>
+	<tr>
+		{#if started}
+			{#each player as p}
+				<td>20</td>
+			{/each}
+		{/if}
+	</tr>
+</tbody>
