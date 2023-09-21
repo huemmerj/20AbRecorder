2147fee70cca623f735fd6d7a96f97fd748125e0
Jens Hümmer <huemmer.jens@gmail.com>
2023-09-21 23:22:29 +0200

    switched game to main page

--- 20AbRecorder/src/routes/+page.svelte
+++ 20AbRecorder/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang=ts>
+	import Header from './Header.svelte';
+	import Body from './Body.svelte';
+	$: started = false;
+	$: players = Array<string>();
+	$: game = Array<Array<number>>();
+	let initGame = () => {
+		started = true;
+		game.push(Array<number>())
+		players.forEach((_) => {
+			game[0].push(20);
+		});
+		game = [...game];
+	};
+</script>
+
+<table>
+	<thead>
+		<th />
+		{#each players as p}
+			<th>{p}</th>
+		{/each}
+		<Header
+			on:addPlayer={(name) => {
+				console.log(players);
+				players = [...players, name.detail];
+			}}
+			started={started}
+		/>
+	</thead>
+	<Body started={started} {game} {players}/>
+</table>
+
+{#if !started}
+	<button on:click={initGame}>Start Game</button>
+{/if}
