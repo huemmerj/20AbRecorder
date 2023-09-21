ae7c175fcdbd036392e1be4f2e8822d2bc78ad71
Jens Hümmer <huemmer.jens@gmail.com>
2023-09-21 22:37:05 +0200

    added Netlify

--- 20AbRecorder/svelte.config.js
+++ 20AbRecorder/svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,10 +7,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			edge: false,
+			split: true
+		})
 	}
 };
 
